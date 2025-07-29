2cb3d2ac2e9eb9fac244a1e24c7443d5018ec9e0
橋谷　崇 <t61342006@yahoo.co.jp>
2025-07-29 09:39:47 +0900

    Conditionの状態を取得するためのプロパティを作成

--- DDD.Domain/ValueObjects/Condition.cs
+++ DDD.Domain/ValueObjects/Condition.cs
@@ -9,6 +9,27 @@ namespace DDD.Domain.ValueObjects
     public sealed class Condition : ValueObject<Condition>
     {
         /// <summary>
+        /// 不明
+        /// </summary>
+        public static readonly Condition None = new Condition(0);
+
+        /// <summary>
+        /// 晴れ
+        /// </summary>
+        public static readonly Condition Sunny = new Condition(1);
+
+        /// <summary>
+        /// 曇り
+        /// </summary>
+        public static readonly Condition Cloudy = new Condition(2);
+
+        /// <summary>
+        /// 雨
+        /// </summary>
+        public static readonly Condition Rain = new Condition(3);
+
+
+        /// <summary>
         /// コンストラクタで状態の値をValueに設定
         /// </summary>
         /// <param name="value">状態の値=数値</param>
@@ -24,20 +45,21 @@ namespace DDD.Domain.ValueObjects
 
         /// <summary>
         /// 状態の表示
+        /// this(コンストラクタのValue)の値で各プロパティを比較し状態("晴れ"など)を返す
         /// </summary>
         public string DisplayValue
         {
             get 
             {  
-                if(this.Value == 1)
+                if(this == Sunny)
                 {
                     return "晴れ";
                 }
-                if (this.Value == 2)
+                if (this == Cloudy)
                 {
                     return "曇り";
                 }
-                if (this.Value == 3)
+                if (this == Rain)
                 {
                     return "雨";
                 }
